46f6772f850c1480e8f7049e9b99d8494e7cea34
yashraj3dot14 <84558238+yashraj3dot14@users.noreply.github.com>
2022-02-20 18:35:28 +0530

    changes in settings.py

--- UserRegistration/settings.py
+++ UserRegistration/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3je2e_y9=o$lnya+p!2b#arzmdgldjvb!i26p7hn+d2g@keoqr'
+SECRET_KEY = '########'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
